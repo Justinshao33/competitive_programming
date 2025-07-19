5813c6e8e3ec334459d669834ecf7ae605a8407f
Justinshao33 <psp940929@gmail.com>
2025-07-19 14:24:11 +0800

    CSES

---
+++ done/CSES/K_th_Highest_Score.cpp
@@ -0,0 +1,88 @@
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG 1
+#endif
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,popcnt,sse4,abm")
+#include<bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rep(X, a, b) for(int X = a; X < b; ++X)
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define ZTMYACANESOCUTE // freopen("in.txt", "r", stdin);
+#define debug(...) {cerr << #__VA_ARGS__ << " = "; dbg(__VA_ARGS__);}
+#else
+#define ZTMYACANESOCUTE ios_base::sync_with_stdio(0), cin.tie(0);
+#define debug(...) 6;
+#endif
+
+void dbg() { cerr << '\n'; }
+template<typename T, typename ...U>
+void dbg(T t, U ...u) { cerr << t << ' '; dbg(u...); }
+
+pii operator + (const pii &p1, const pii &p2) { return make_pair(p1.fi + p2.fi, p1.se + p2.se); }
+pii operator - (const pii &p1, const pii &p2) { return make_pair(p1.fi - p2.fi, p1.se - p2.se); }
+pll operator + (const pll &p1, const pll &p2) { return make_pair(p1.fi + p2.fi, p1.se + p2.se); }
+pll operator - (const pll &p1, const pll &p2) { return make_pair(p1.fi - p2.fi, p1.se - p2.se); }
+
+template<class T> bool chmin(T &a, T b) { return (b < a && (a = b, true)); }
+template<class T> bool chmax(T &a, T b) { return (a < b && (a = b, true)); }
+
+#define lpos pos << 1
+#define rpos pos << 1 | 1
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << "," << p.second << ')'; }
+template<typename A> ostream& operator << (ostream &os, const vector<A> &p) { for(const auto &a : p) os << a << " "; os << '\n'; return os; }
+ 
+const int MAXN = 2e5 + 5, MOD = 998244353, IINF = 1e9 + 7, MOD2 = 1000000007;
+const ll LINF = 1e18L + 5;
+const int B = 320;
+
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// int get_rand(int l, int r){ return uniform_int_distribution<int>(l, r)(rng); }
+ 
+ll fpow (ll x, ll exp, ll mod = LLONG_MAX) { if (x == 0) return 0; ll res = 1; while (exp > 0) { if (exp & 1) res = res * x % mod; x = x * x % mod; exp >>= 1; } return res; }
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<ll> F(n + 2, LINF), S(n + 2, LINF);
+    F[n + 1] = S[n + 1] = 0;
+    auto ask = [&](int x) -> void {
+        if (x > 0 && x <= n && F[x] == LINF) {
+            cout << "F " << x << '\n';
+            cin >> F[x];
+        }
+        if (k - x > 0 && k - x <= n && S[k - x] == LINF) {
+            cout << "S " << k - x << '\n';
+            cin >> S[k - x];
+        }
+    };
+    int l = max(0, k - n) - 1, r = min(n, k);
+    while (r - l > 1) {
+        int mid = l + r >> 1;
+        ask(mid);
+        ask(mid + 1);
+        if (F[mid + 1] >= S[k - mid]) l = mid;
+        else r = mid;
+    }
+    ask(r);
+    cout << "! " << min(F[r], S[k - r]) << '\n';
+}
+ 
+int main() {
+    // ZTMYACANESOCUTE;
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+}
